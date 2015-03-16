75d54a63b13d2f69b4ef8241e9b3299b673373a4
davlin93 <dlin@clypd.com>
2015-03-15 21:25:21 -0400

    move DOM bindings to main file to prevent clutter in parse module

--- js/parse.js
+++ js/parse.js
@@ -10,61 +10,6 @@
 */
 
 $(function() {
-  var directory = new air.File();
-  var picturePath = "";
-  var ratingsFile;
-  var pictures = [];
-
-  /*
-    When the #picture-directory input is clicked, use AIR's browseForDirectory() method
-    When the directory is selected, save the path and display it in the form.
-  */
-  $('#picture-directory').click(function(e) {
-    e.preventDefault();
-    directory.addEventListener(air.Event.SELECT, function(e) {
-      picturePath = directory.nativePath;
-      $('#picture-directory').text(picturePath);
-    });
-    directory.browseForDirectory("Choose the picture directory");
-  });
-
-  /*
-    When submit is clicked on the form, parse all of the input into variables
-    and call parse() to parse the ratings file.
-  */
-  $('#input-form').submit(function(e) {
-    e.preventDefault();
-    air.trace("parsing: " + $('#ratings-file').val());
-    ratingsFile = new air.File($('#ratings-file').val());
-    numGroups = $('#num-groups').val();
-    picsPerGroup = $('#pics-per-group').val();
-    avgRating = $('#rating-per-group').val();
-    air.trace("numGroups: " + numGroups + ", picsPerGroup: " + picsPerGroup + ", avgRating: " + avgRating);
-    pictures = parse();
-
-    splitFunc = 'ra'
-
-    air.trace("Splitting with algorithm: " + splitFunc);
-
-    groups = Stats.split({
-      numGroups: numGroups,
-      numPictures: picsPerGroup,
-      targetRating: avgRating,
-      pictures: pictures,
-      splitFunc: splitFunc
-    });
-
-    i = 1;
-    groups.forEach(function(group) {
-      air.trace("Group " + i);
-      group.forEach(function(picture) {
-        air.trace("filePath: " + picture.filePath + ", rating: " + picture.rating);
-      });
-
-      i++;
-    });
-  });
-
   /*
     Open the file that was input and parse it into an array of picture objects
   */
