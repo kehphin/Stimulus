8db3d301c6afcfccc40eb4504ccd29ea0fdf3bf8
Tony Huang <tony.huang.jun@gmail.com>
2015-03-16 17:03:10 -0400

    reformatted stats test file

--- js/__tests__/stats-test.js
+++ js/__tests__/stats-test.js
@@ -14,18 +14,36 @@ jest.dontMock("../picture.js");
 jest.dontMock("../validate.js");
 jest.dontMock("../underscore-min.js");
 
-// Mock Data
+// Requires 
 var Stats = require("../stats.js");
 var Picture = require("../picture.js");
 var _ = require("../underscore-min.js");
+
+
+// Mock Data
 var emptyPictures = [];
+
 var mockPictures = [new Picture(7, "foo")];
+
 var mockPictures2 = [new Picture(5, "foo"), 
   new Picture(7, "bar"), 
   new Picture(9, "baz"),
   new Picture(9, "qux")];
 
-// Tests for our stats functions.
+var mockPictures3 = [
+  new Picture(0, "Picture 0"),
+  new Picture(1, "Picture 1"), 
+  new Picture(2, "Picture 2"),
+  new Picture(3, "Picture 3"),
+  new Picture(4, "Picture 4"), 
+  new Picture(5, "Picture 5"),
+  new Picture(6, "Picture 6"),
+  new Picture(7, "Picture 7"), 
+  new Picture(8, "Picture 8"),
+  new Picture(9, "Picture 9")
+]
+
+
 describe("statistics", function() {
   it("computes the median rating", function() {
     expect(Stats.medianRating(mockPictures)).toBe(7);
@@ -72,117 +90,6 @@ describe("statistics", function() {
   });
 
 });
-
-var mockPictures3 = [
-  new Picture(0, "Picture 0"),
-  new Picture(1, "Picture 1"), 
-  new Picture(2, "Picture 2"),
-  new Picture(3, "Picture 3"),
-  new Picture(4, "Picture 4"), 
-  new Picture(5, "Picture 5"),
-  new Picture(6, "Picture 6"),
-  new Picture(7, "Picture 7"), 
-  new Picture(8, "Picture 8"),
-  new Picture(9, "Picture 9")
-]
-
-
-
-
-
-
-// Tests for our split algorithm (GREEDY)
-describe("greedy split", function() {
-  it("greedy splits the data set", function() {
-    var splitPictures = Stats.split({
-      "numGroups": 2, 
-      "numPictures": 1, 
-      "targetRating": 9, 
-      "pictures": mockPictures2, 
-      "splitFunc": "gr"
-    });
-
-    var expected = [
-      [new Picture(9, "baz")], 
-      [new Picture(9, "qux")]
-    ];
-
-    expect(splitPictures).toEqual(expected);
-  });
-
-  it("also greedy splits the data set", function() {
-    var splitPictures = Stats.split({
-      "numGroups": 2, 
-      "numPictures": 1, 
-      "targetRating": 4.5, 
-      "pictures": mockPictures3, 
-      "splitFunc": "gr"
-    });
-
-    var expected = [
-      [new Picture(4, "Picture 4")],
-      [new Picture(5, "Picture 5")]
-    ];
-
-    expect(splitPictures).toEqual(expected);
-  });
-
-  it("greedy splits the data set with more pictures", function() {
-    var splitPictures = Stats.split({
-      "numGroups": 2, 
-      "numPictures": 3, 
-      "targetRating": 4.5, 
-      "pictures": mockPictures3, 
-      "splitFunc": "gr"
-    });
-
-    var expected = [
-      [
-        new Picture(4, "Picture 4"),
-        new Picture(5, "Picture 5"),
-        new Picture(3, "Picture 3")
-      ],
-      [
-        new Picture(6, "Picture 6"),
-        new Picture(2, "Picture 2"),
-        new Picture(7, "Picture 7")
-      ]
-    ];
-
-    expect(splitPictures).toEqual(expected);
-  });
-
-  it("greedy splits the data set into 3 groups", function() {
-    var splitPictures = Stats.split({
-      "numGroups": 3, 
-      "numPictures": 3, 
-      "targetRating": 4.5, 
-      "pictures": mockPictures3, 
-      "splitFunc": "gr"
-    });
-
-    var expected = [
-      [
-        new Picture(4, "Picture 4"),
-        new Picture(5, "Picture 5"),
-        new Picture(3, "Picture 3")
-      ],
-      [
-        new Picture(6, "Picture 6"),
-        new Picture(2, "Picture 2"),
-        new Picture(7, "Picture 7")
-      ],
-      [
-        new Picture(1, "Picture 1"),
-        new Picture(8, "Picture 8"),
-        new Picture(0, "Picture 0")
-      ]
-    ];
-
-    expect(splitPictures).toEqual(expected);
-  });
-});
-
 
 
 describe("greedy split", function() {
@@ -275,13 +182,6 @@ describe("greedy split", function() {
     expect(splitPictures).toEqual(expected);
   });
 });
-
-
-
-
-
-
-
 
 
 describe("round robin split", function() {
@@ -374,8 +274,6 @@ describe("round robin split", function() {
     expect(splitPictures).toEqual(expected);
   });
 });
-
-
 
 
 describe("random split", function() {
