1b767a82aae32d623ef7022be9803d1093e09845
gary2546 <song.ch@husky.neu.edu>
2015-03-31 21:42:05 -0400

    createChart function

--- js/mock_data.js
+++ js/mock_data.js
@@ -1,6 +1,13 @@
-// Creates mock data to make initial fusion chart
-// num_pics is an int : number of pictures
-// data_range is a array of size 2 : [min, max]
+/*
+Author: Gary Song
+Group: Stimulus
+Created on: 1/31/2015
+
+Modified on: 3/31/2015
+
+Creates a mock ratings list of size num_pics,
+which has data between 0 to data_ra
+*/
 function createMockChartData(num_pics, data_range) {
 
   // Returns a random number between min (inclusive) and max (exclusive)
@@ -18,63 +25,6 @@ function createMockChartData(num_pics, data_range) {
   pics_data = _.sortBy(pics_data, function(num) {
       return num;
   });
-  
-  var pic_num = 0;
-  // converts pics_data into a form fusioncharts use as input
-  var mock_data = _.map(pics_data, function(pic_data) {
-    pic_num++;
-    return [pic_num, pic_data];
-  });
-  
-  // creates fusioncharts input json
-  var chart_data = {
-    "chart": {
-      "type": "scatter",
-      "zoomType": "xy"
-    },
-    "legend": {
-      "enabled": false
-    },
-    "xAxis": {
-      "title": {
-        "text": "picture"
-      }
-    },
-    "yAxis": {
-      "title": {
-        "text": "rating"
-      }
-    },
-    "plotOptions": {
-      "scatter": {
-        "marker": {
-          "radius": 5,
-          "states": {
-            "hover": {
-              "enabled": true,
-              "lineColor": "rgb(100,100,100)"
-            }
-          }
-        },
-        "states": {
-          "hover": {
-            "marker": {
-              "enabled": false
-            }
-          }
-        },
-        "tooltip": {
-          "headerFormat": '<b>{series.name}</b><br>',
-          "pointFormat": '{point.y}'
-        }
-      }
-    },
-    "series": [{
-      "name": "Picture",
-      "color": "rgba(119, 152, 191, .5)",
-      "data": mock_data
-    }]
-  };
-  
-  return chart_data;
+
+  return pics_data;
 }
